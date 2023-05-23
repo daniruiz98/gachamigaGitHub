64cca315f24bf15021f2cd57ccc06307da611103
Daniel Ruiz Miralles <danimajo98@gmail.com>
2023-05-23 11:32:10 +0200

    Test de los caminos basicos desde V3

--- Test/Test1.java
+++ Test/Test1.java
@@ -10,21 +10,9 @@ public class Test1 {
     }
 
     @Test
-    public void testFor2(){
-        String result = V3.catar(2);
-        Assertions.assertEquals("2", result);
-    }
-
-    @Test
     public void testFor3(){
         String result = V3.catar(3);
         Assertions.assertEquals("Gacha", result);
-    }
-
-    @Test
-    public void testFor4(){
-        String result = V3.catar(4);
-        Assertions.assertEquals("4", result);
     }
 
     @Test
@@ -34,70 +22,11 @@ public class Test1 {
     }
 
     @Test
-    public void testFor6(){
-        String result = V3.catar(6);
-        Assertions.assertEquals("Gacha", result);
-    }
-
-    @Test
-    public void testFor7(){
-        String result = V3.catar(7);
-        Assertions.assertEquals("7", result);
-    }
-
-    @Test
-    public void testFor8(){
-        String result = V3.catar(8);
-        Assertions.assertEquals("8", result);
-    }
-
-    @Test
-    public void testFor9(){
-        String result = V3.catar(9);
-        Assertions.assertEquals("Gacha", result);
-    }
-
-    @Test
-    public void testFor10(){
-        String result = V3.catar(10);
-        Assertions.assertEquals("miga", result);
-    }
-
-    @Test
-    public void testFor11(){
-        String result = V3.catar(11);
-        Assertions.assertEquals("11", result);
-    }
-
-    @Test
-    public void testFor12(){
-        String result = V3.catar(12);
-        Assertions.assertEquals("Gacha", result);
-    }
-
-    @Test
-    public void testFor13(){
-        String result = V3.catar(13);
-        Assertions.assertEquals("13", result);
-    }
-
-    @Test
-    public void testFor14(){
-        String result = V3.catar(14);
-        Assertions.assertEquals("14", result);
-    }
-
-    @Test
     public void testFor15(){
         String result = V3.catar(15);
         Assertions.assertEquals("Gachamiga", result);
     }
 
-    @Test
-    public void testFor16(){
-        String result = V3.catar(30);
-        Assertions.assertEquals("Gachamiga", result);
-    }
 
 
 
