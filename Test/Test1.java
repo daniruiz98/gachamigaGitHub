4521b56abc0d1e21fd958373eb0c2024cc581f3b
Daniel Ruiz Miralles <danimajo98@gmail.com>
2023-05-23 11:42:34 +0200

    16 Tests pasados desde V4

--- Test/Test1.java
+++ Test/Test1.java
@@ -9,16 +9,86 @@ public class Test1 {
         Assertions.assertEquals("1", result);
     }
 
+
+    @Test
+    public void testFor2(){
+        String result = Gachamiguero.preparar(2);
+        Assertions.assertEquals("2", result);
+    }
+
+
     @Test
     public void testFor3(){
         String result = Gachamiguero.preparar(3);
         Assertions.assertEquals("Gacha", result);
     }
 
+
+    @Test
+    public void testFor4(){
+        String result = Gachamiguero.preparar(4);
+        Assertions.assertEquals("4", result);
+    }
+
     @Test
     public void testFor5(){
         String result = Gachamiguero.preparar(5);
         Assertions.assertEquals("miga", result);
+    }
+    @Test
+    public void testFor6(){
+        String result = Gachamiguero.preparar(6);
+        Assertions.assertEquals("Gacha", result);
+    }
+
+    @Test
+    public void testFor7(){
+        String result = Gachamiguero.preparar(7);
+        Assertions.assertEquals("7", result);
+    }
+
+
+    @Test
+    public void testFor8(){
+        String result = Gachamiguero.preparar(8);
+        Assertions.assertEquals("8", result);
+    }
+
+    @Test
+    public void testFor9(){
+        String result = Gachamiguero.preparar(9);
+        Assertions.assertEquals("Gacha", result);
+    }
+
+
+    @Test
+    public void testFor10(){
+        String result = Gachamiguero.preparar(10);
+        Assertions.assertEquals("miga", result);
+    }
+
+    @Test
+    public void testFor11(){
+        String result = Gachamiguero.preparar(11);
+        Assertions.assertEquals("11", result);
+    }
+
+    @Test
+    public void testFor12(){
+        String result = Gachamiguero.preparar(12);
+        Assertions.assertEquals("Gacha", result);
+    }
+
+    @Test
+    public void testFor13(){
+        String result = Gachamiguero.preparar(13);
+        Assertions.assertEquals("13", result);
+    }
+
+    @Test
+    public void testFor14(){
+        String result = Gachamiguero.preparar(14);
+        Assertions.assertEquals("14", result);
     }
 
     @Test
@@ -27,6 +97,11 @@ public class Test1 {
         Assertions.assertEquals("Gachamiga", result);
     }
 
+    @Test
+    public void testFor16(){
+        String result = Gachamiguero.preparar(30);
+        Assertions.assertEquals("Gachamiga", result);
+    }
 
 
 
